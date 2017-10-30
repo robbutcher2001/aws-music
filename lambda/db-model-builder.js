3279b58f8905fb65cc6dbfa5299cb298716cd6bc
Rob Butcher <robbutcher2001@gmail.com>
2017-10-30 17:18:42 +0000

    Reading S3 music bucket

--- lambda/db-model-builder.js
+++ lambda/db-model-builder.js
@@ -1,18 +1,32 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
-  const params = {
-    TableName : 'Artists',
-    Item: {
-      'hello': 'world'
-    }
-  };
+const trackKeyParams = {
+  Bucket: process.env.TRACK_BUCKET
+};
 
-  documentClient.put(params, function(err, data) {
-    if (err) console.log(err);
-    else console.log(data);
+const getTrackKeys = () => {
+  s3.listObjectsV2(trackKeyParams, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
   });
+};
+
+exports.handler = (event, context, callback) => {
+  // const params = {
+  //   TableName : 'Artists',
+  //   Item: {
+  //     'hello': 'world'
+  //   }
+  // };
+  //
+  // documentClient.put(params, function(err, data) {
+  //   if (err) console.log(err);
+  //   else console.log(data);
+  // });
+
+  getTrackKeys();
 };
