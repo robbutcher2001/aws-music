c0172fe0254f64612f92a4dda43c5c9cec990edd
Rob Butcher <robbutcher2001@gmail.com>
2017-10-31 17:38:10 +0000

    Getting all track keys from bucket

--- lambda/db-model-builder.js
+++ lambda/db-model-builder.js
@@ -8,14 +8,46 @@ const trackKeyParams = {
   Bucket: process.env.TRACK_BUCKET
 };
 
-const getTrackKeys = () => {
-  s3.listObjectsV2(trackKeyParams, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
+//TODO: recurse if data.IsTruncated === true
+const getTrackKeys = () =>
+  new Promise((resolve, reject) => {
+    const trackKeys = [];
+
+    s3.listObjectsV2(trackKeyParams, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        data.Contents.forEach(track => {
+          trackKeys.push(track.Key);
+        });
+
+        resolve(trackKeys);
+      }
+    });
   });
+
+const getTrackTags = trackKeys => {
+  trackKeys.forEach(trackKey => {
+    console.log(trackKey);
+  });
+
+  // const headObjectParams = {
+  //   Bucket: process.env.TRACK_BUCKET,
+  //   Key: data.Contents[0].Key
+  // };
+  // s3.getObjectTagging(headObjectParams, function(err, data) {
+  //   if (err) console.log(err, err.stack); // an error occurred
+  //   else     console.log(data);           // successful response
+  // });
 };
 
 exports.handler = (event, context, callback) => {
+  getTrackKeys()
+    .then(trackKeys => {
+      console.log(JSON.stringify(trackKeys));
+    })
+    .catch(callback());
   // const params = {
   //   TableName : 'Artists',
   //   Item: {
@@ -27,6 +59,4 @@ exports.handler = (event, context, callback) => {
   //   if (err) console.log(err);
   //   else console.log(data);
   // });
-
-  getTrackKeys();
 };
