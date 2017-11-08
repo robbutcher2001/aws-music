f4ffa291826c737bb959657c6c3d9efcfbd6baa5
Rob Butcher <robbutcher2001@gmail.com>
2017-11-08 10:51:03 +0000

    Removing old code

--- lambda/db-model-builder.js
+++ lambda/db-model-builder.js
@@ -7,12 +7,7 @@ const trackBucket = process.env.TRACK_BUCKET;
 const createTrack = trackKey =>
   new Promise((resolve, reject) => {
     const track = trackKey;
-    const tagParams = {
-      Bucket: trackBucket,
-      Key: track.key
-    };
-
-    getS3ObjectTags(tagParams)
+    getS3ObjectTags({Bucket: trackBucket, Key: track.key})
       .then(trackTags => {
         trackTags.TagSet.forEach(trackTag => {
           track[`${trackTag.Key}`] = trackTag.Value;
@@ -39,42 +34,6 @@ const getArtistAlbums = (tracksFlatList, artist) => {
 const getAlbumTracks = (tracksFlatList, artist, album) =>
   tracksFlatList.filter(track => track.artist === artist && track.album === album);
 
-const searchObject = (obj, term) => {
-  var found = false;
-  Object.keys(obj).forEach(key => {
-    console.log(`Looking at key: ${key} and object key: ${obj[key]}`);
-    found = (found || term === obj[key]) ? true : false;
-  });
-
-  return found;
-};
-
-const getLibraryArtist = (library, artist) => {
-  var found;
-  library.forEach(item => {
-    if (item.artist === artist) {
-      found = item;
-    }
-  });
-
-  return found;
-};
-
-const getLibraryAlbum = (library, artist, album) => {
-  var found;
-  library.forEach(item => {
-    if (item.artist === artist) {
-      item.albums.forEach(existingAlbum => {
-        if (existingAlbum.name === album) {
-          found = item;
-        }
-      });
-    }
-  });
-
-  return found;
-};
-
 const buildDbModel = tracksFlatList => {
   const library = [];
 
@@ -88,61 +47,6 @@ const buildDbModel = tracksFlatList => {
         album.tracks.push({title: trackItem.title, year: trackItem.year});
       });
     });
-  });
-
-  return library;
-};
-
-const buildLibrary = tracks => {
-  const library = [];
-
-  // Build artists
-  tracks.forEach(track => {
-    if (typeof track.artist !== 'undefined') {
-      const item = getLibraryArtist(library, track.artist);
-      if (typeof item === 'undefined') {
-        const item = {
-          artist: track.artist,
-          albums: []
-        };
-        library.push(item);
-      }
-      else {
-        console.log(`${track.artist} already in library`);
-      }
-    }
-  });
-
-  // Build albums
-  tracks.forEach(track => {
-    const item = getLibraryAlbum(library, track.artist, track.album);
-    if (typeof item === 'undefined') {
-      const artist = getLibraryArtist(library, track.artist);
-      if (typeof artist !== 'undefined') {
-        artist.albums.push({
-          name: track.album,
-          tracks: []
-        });
-      }
-    }
-    else {
-      console.log(`${track.album} already in library`);
-    }
-  });
-
-  // Add tracks
-  tracks.forEach(track => {
-    const item = getLibraryAlbum(library, track.artist, track.album);
-    if (typeof item !== 'undefined') {
-      const album = item.albums.find(album => album.name === track.album);
-      album.tracks.push({
-        title: track.title,
-        year: track.year
-      });
-    }
-    else {
-      console.log(`${track.title} doesn't have a place to go in library`);
-    }
   });
 
   return library;
