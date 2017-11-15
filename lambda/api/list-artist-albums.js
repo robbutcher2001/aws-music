3ec806c78345e62fa167df5a56ec0d5f30ed174a
Rob Butcher <robbutcher2001@gmail.com>
2017-11-15 12:12:00 +0000

    Feeding artistId into function

--- lambda/api/list-artist-albums.js
+++ lambda/api/list-artist-albums.js
@@ -3,12 +3,9 @@
 const { getArtistService } = require('../services');
 
 exports.handler = (event, context, callback) => {
-  const artistId = event.params;
-  console.log(JSON.stringify(artistId));
-
   const response = {};
 
-  getArtistService('dd10bcc3-04f7-4930-bcbb-8056358b89fa')
+  getArtistService(event.params.artistId)
     .then(albums => {
       response.status = 'success';
       response.data = albums;
