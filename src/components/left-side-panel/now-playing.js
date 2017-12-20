ae127fabeb597d06cb95060753d9a6a0e147a2e5
Rob Butcher <robbutcher2001@gmail.com>
2017-12-20 16:47:17 +0000

    Now playing context title

--- src/components/left-side-panel/now-playing.js
+++ src/components/left-side-panel/now-playing.js
@@ -8,6 +8,7 @@ export default class NowPlaying extends Component {
           <img src="images/cb.png" alt="" />
         </span>
         <h1 id="title">Now playing</h1>
+        <p>Alternative Rock | 2014</p>
       </div>
     );
   }
