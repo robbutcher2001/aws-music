8a6e0cb83bbf6e88e6ff34d6558bbbdffdf655cb
Rob Butcher <robbutcher2001@gmail.com>
2018-01-04 12:22:51 +0000

    Removing old js

--- src/boilerplate-assets/js/butch-mockup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	setTimeout(function() {
-		document.getElementById('loading').className = 'hide';
-		document.getElementById('artist-list').className = '';
-	}, 2000);
-});
